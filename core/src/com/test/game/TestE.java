c2ed63ea5d06a78fb6ade14defab4fd7d2effc86
retropalladin <vbel9018128@outlook.com>
2018-03-20 20:50:38 +0300

    MyTestGame modded

--- core/src/com/test/game/TestE.java
+++
@@ -1,8 +0,0 @@
-package com.test.game;
-
-/**
- * Created by Егор on 20.03.2018.
- */
-
-public class TestE {
-}
--- core/src/com/test/game/TestV.java
+++
@@ -1,8 +0,0 @@
-package com.test.game;
-
-/**
- * Created by Владимир on 20.03.2018.
- */
-
-public class TestV {
-}
